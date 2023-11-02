0e31b57af3337d13e1204a132de1224a08c604fb
Luana <luanadevops@gmail.com>
2023-11-02 15:17:49 -0300

    updates at layout

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask, render_template, request, redirect, session, flash
 from pymongo import MongoClient
 import bcrypt
 from datetime import datetime
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.secret_key = "sua_chave_secreta"  # Substitua pela sua chave secreta
-client = MongoClient('mongodb+srv://luanavidalnn:EhOUJpUjYvKPfQUs@cluster01.dxsxqa5.mongodb.net/')  # Atualize a URL do MongoDB, se necessário
+app.secret_key = "sua_chave_secreta"
+client = MongoClient('mongodb+srv://luanavidalnn:Uu5XcplTXslu5YZE@cluster01.dxsxqa5.mongodb.net/'
+                    '?retryWrites=true&w=majority')
 db = client['diario_de_obras']
 entries = db.entries
-users = db.users  # Coleção de usuários
+users = db.users
+works = db.works
+
+def is_admin():
+    if 'username' in session:
+        username = session['username']
+        user = users.find_one({'username': username})
+        return user.get('profile') == 'administrador'
+    return False
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        flash('Faça login para acessar esta página.', 'danger')
+        return redirect('/login')
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        username = session['username']
-        entries_list = entries.find({'created_by': username})
-        return render_template('diary.html', entries=entries_list)
-    else:
-        flash('Faça login para acessar o diário.', 'danger')
-        return redirect('/login')
-
+    username = session['username']
+    entries_list = entries.find({'created_by': username})
+    return render_template('diary.html', entries=entries_list)
 
 @app.route('/add_entry', methods=['POST'])
 def add_entry():
-    # Verifica se o usuário está logado
     if 'username' in session:
         username = session['username']
         title = request.form.get('title')
@@ -31,18 +42,40 @@ def add_entry():
         entry_date = request.form.get('entry_date')
         entry_time = request.form.get('entry_time')
 
-        # Define entry_datetime com um valor padrão
-        entry_datetime = None
-
-        # Verifica se entry_date e entry_time foram fornecidos
-        if entry_date and entry_time:
-            entry_datetime = datetime.strptime(f"{entry_date} {entry_time}", "%Y-%m-%d %H:%M")
+        entry_datetime = datetime.strptime(f"{entry_date} {entry_time}", "%Y-%m-%d %H:%M")
 
         entries.insert_one({'title': title, 'description': description, 'entry_datetime': entry_datetime, 'created_by': username})
+        flash('Entrada adicionada com sucesso!', 'success')
         return redirect('/')
     else:
         flash('Faça login para adicionar uma entrada.', 'danger')
         return redirect('/login')
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    if is_admin():
+        # Adicione o usuário
+        return redirect('/users')
+
+# Editar um usuário
+@app.route('/users/<username>', methods=['PUT'])
+def edit_user(username):
+    if is_admin():
+        # Atualize o usuário
+        return redirect('/users')
+
+# Remover um usuário
+@app.route('/users/<username>', methods=['DELETE'])
+def delete_user(username):
+    if is_admin():
+        # Remova o usuário
+        return redirect('/users')
+
+
+@app.route('/report/works')
+def report_works():
+    works_list = works.find()
+    return render_template('report_works.html', works=works_list)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -50,7 +83,7 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password').encode('utf-8')
         hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
-        
+
         if users.find_one({'username': username}):
             flash('Nome de usuário já existe.', 'danger')
         else:
@@ -60,37 +93,106 @@ def register():
 
     return render_template('register.html')
 
+@app.route('/manage_users')
+def manage_users():
+    print("Acessou a rota de gerenciamento de usuarios")
+    if is_admin():
+        users_list = users.find()
+        return render_template('manage_users.html', users=users_list)
+    else:
+        flash('Acesso permitido apenas para o administrador.', 'danger')
+        return redirect('/')
+
+@app.route('/manage_works')
+def manage_works():
+    if is_admin():
+        works_list = works.find()
+        return render_template('works_list.html', works=works_list)
+    else:
+        flash('Acesso permitido apenas para o administrador.', 'danger')
+        return redirect('/')
+    
+@app.route('/works')
+def list_works():
+    if 'username' in session:
+        username = session['username']
+        user = users.find_one({'username': username})
+
+        if user.get('profile') == 'admin':
+            # Consulte as obras no banco de dados e passe-as para o modelo
+            works_list = works.find()
+            return render_template('works_list.html', works=works_list)
+        else:
+            flash('Acesso permitido apenas para o administrador.', 'danger')
+            return redirect('/')
+    else:
+        flash('Faça login para acessar esta página.', 'danger')
+        return redirect('/login')
+
+@app.route('/works/add', methods=['POST'])
+def add_work():
+    if is_admin():
+        work_name = request.form.get('work_name')
+        works.insert_one({'work_name': work_name})
+        flash('Obra cadastrada com sucesso!', 'success')
+        return redirect('/works')
+    else:
+        flash('Acesso permitido apenas para o administrador.', 'danger')
+        return redirect('/')
+    
+@app.route('/works/edit/<work_id>', methods=['POST'])
+def edit_work(work_id):
+    if is_admin():
+        new_work_name = request.form.get('new_work_name')
+        works.update_one({'_id': ObjectId(work_id)}, {"$set": {'work_name': new_work_name}})
+        flash('Obra atualizada com sucesso!', 'success')
+        return redirect('/works')
+    else:
+        flash('Acesso permitido apenas para o administrador.', 'danger')
+        return redirect('/')
+    
+@app.route('/works/delete/<work_id>')
+def delete_work(work_id):
+    if is_admin():
+        works.delete_one({'_id': ObjectId(work_id)})
+        flash('Obra excluída com sucesso!', 'success')
+        return redirect('/works')
+    else:
+        flash('Acesso permitido apenas para o administrador.', 'danger')
+        return redirect('/')
+
 @app.route('/report')
 def report():
-    if 'username' in session:
+    if is_admin():
         entries_list = entries.find()
         return render_template('report.html', entries=entries_list)
     else:
-        flash('Faça login para acessar o relatório.', 'danger')
-        return redirect('/login')
+        flash('Acesso permitido apenas para o administrador.', 'danger')
+        return redirect('/')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password').encode('utf-8')
-        
+
         user = users.find_one({'username': username})
-        
+
         if user and bcrypt.checkpw(password, user['password']):
             session['username'] = username
             flash('Login bem-sucedido!', 'success')
             return redirect('/')
         else:
             flash('Credenciais inválidas. Tente novamente.', 'danger')
-    
+
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     flash('Você foi desconectado.', 'info')
-    return redirect('/login')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
